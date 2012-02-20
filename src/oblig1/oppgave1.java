ed94bd5a43b8e8ec69d173910649a14ab7a34aa5
Kristoffer Berdal <web@flexd.net>
2012-02-20 12:04:04 +0100

    initial commit

---
+++ src/oblig1/oppgave1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oblig1;
+
+/**
+ *
+ * @author Kristoffer Berdal <web@flexd.net>
+ */
+public class oppgave1 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
