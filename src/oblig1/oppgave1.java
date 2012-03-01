385d80df65792e90db70c08b424cd099534accd5
Even Augdal <even.augdal@gmail.com>
2012-03-01 16:54:34 +0100

    Siste finish på oppgave 1

--- src/oblig1/oppgave1.java
+++ src/oblig1/oppgave1.java
@@ -1,5 +1,7 @@
 package oblig1;
 
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author Kristoffer Berdal <web@flexd.net>
@@ -9,7 +11,7 @@ public class oppgave1 {
   /**
    * @param args the command line arguments
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     FileManager f = new FileManager();
     
     f.askFile();
