1ff0d94a52027dc61cad0fa356997542be725c73
Kristoffer Berdal <web@flexd.net>
2012-03-01 17:56:24 +0100

    Info om oss

--- src/oblig1/oppgave1.java
+++ src/oblig1/oppgave1.java
@@ -1,11 +1,22 @@
+/**
+ *
+ * @author Kristoffer Berdal <web@flexd.net>
+ * @studnr 180212
+
+ * @author Even Augdal <even.augdal@gmail.com>
+ * @studnr 181091
+ * 
+ * @author Tommy Nyrud <s180487@stud.hioa.no>
+ * @studnr 180487
+ * 
+ * @date Feb 20, 2012
+ */
+
 package oblig1;
 
 import java.io.FileNotFoundException;
 
-/**
- *
- * @author Kristoffer Berdal <web@flexd.net>
- */
+
 public class oppgave1 {
 
   /**
